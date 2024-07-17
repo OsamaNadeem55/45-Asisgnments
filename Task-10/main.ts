fa2fb2492d3ec38051c7722ccb5038d18a525c37
OsamaNadeem55 <160299217+OsamaNadeem55@users.noreply.github.com>
2024-07-17 08:55:24 -0700

    45-ASISGNMENT

---
+++ Task-10/main.ts
@@ -0,0 +1,7 @@
+//  Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+const num1:number=30;
+const num2:number=8;
+ 
+// this will add the num 1 and num 2 (this is comment in typescript. We comment like this in typescript)
+console.log(num1+num2);
---
+++ Task-11/main.js
@@ -0,0 +1,6 @@
+// Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var friendsname = ["Ahmed", "Hamza", "Mubashir", "Shoaib"];
+console.log(friendsname[0]);
+console.log(friendsname[1]);
+console.log(friendsname[2]);
+console.log(friendsname[3]);
