540f9fa6c0bd6d27a06a131886fd710e829de16b
OsamaNadeem55 <160299217+OsamaNadeem55@users.noreply.github.com>
2024-07-17 08:54:02 -0700

    Delete Task-10 directory

--- Task-10/main.js
+++
@@ -1,5 +0,0 @@
-//  Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
-var num1 = 30;
-var num2 = 8;
-// this will add the num 1 and num 2 (this is comment in typescript. We comment like this in typescript)
-console.log(num1 + num2);
